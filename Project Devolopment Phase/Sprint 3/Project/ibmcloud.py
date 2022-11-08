42e687c452c9bd2134fc4be38daf9dd3c7daf846
srinath52 <113348820+srinath52@users.noreply.github.com>
2022-11-08 15:43:45 +0530

    Add files via upload

--- Project Devolopment Phase/Sprint 3/Project/ibmcloud.py
+++ Project Devolopment Phase/Sprint 3/Project/ibmcloud.py
@@ -2,10 +2,10 @@ from ibm_watson_machine_learning import APIClient
 
 web_cred={
     "apikey": "6PcwxslwN5BT5dP8C4oKx62Vsz661Sxw-KY3oVAygalC",
-	"url": "https://eu-gb.ml.cloud.ibm.com"
+	"url": "https://cloud.ibm.com/user"
 }
 
 client=APIClient(web_cred)
-spaceID="e4ef5efc-e11a-41cc-9c1e-b45c6a510aeb"
+spaceID="e4et5ffc-r11a-4hcc-9c1e-r45c6a510aeb"
 x=client.set.default_space(spaceID)
 print(x)
